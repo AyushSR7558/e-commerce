23f012d80c9e18e209e726424d6e719e4666ccf5
Ayush <ayushraut7558@gmail.com>
2026-01-12 14:05:54 +0530

    Configure the gateway

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -3,12 +3,46 @@
  * This is only a minimal backend to get started.
  */
 
-import express from 'express';
+import express, { urlencoded } from 'express';
 import * as path from 'path';
-
+import cors from 'cors';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser'
+import rateLimit, { ipKeyGenerator } from 'express-rate-limit';
 const app = express();
 
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+    allowedHeaders: ['Authorization', 'Authentication', 'Content-Type'],
+  }),
+);
+
+// app.use(morgan('dev')); // This is the middle ware which will log the req
+// app.use(express.json({ limit: '100mb' }));// We are limiting the size of the request body
+// app.use(urlencoded({ limit: '100mb', extended: true }));
+// app.use(cookieParser());// Parsing the cookie 
+// app.set("trust proxy", 1);
+
+  app.use(morgan('dev'));// This will log the request. So that it will be easier for debugging
+  app.use(express.json({limit:'100mb'})); // We are limiting the size of the request body
+  app.use(urlencoded({limit: '100mb', extended: true}));
+  app.set("trust proxy", 1);
+  app.use(cookieParser());
+
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: (req) => 100,
+  message: { error: "Too many requests, please try again later" },
+  standardHeaders: true,
+  legacyHeaders: false,
+  keyGenerator: (req) => req.ip!,
+});
+
+app.use(limiter);
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to api-gateway!' });
